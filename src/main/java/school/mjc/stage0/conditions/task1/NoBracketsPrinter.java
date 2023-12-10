5209820f480fd4f7f901027c500d34e958b0220c
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-10 09:58:34 +0500

    finished

--- src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
@@ -4,6 +4,6 @@ public class NoBracketsPrinter {
     public static void main(String[] args) {
         if(false)
             System.out.println("The statement is true");
-        System.out.println("I'm out of checking");
+        else return; System.out.println("I'm out of checking");
     }
 }
